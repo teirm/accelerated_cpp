4ee63020d7ba1404e588dcd3929b625ee4a8c9ad
teirm <tribunus.vii@gmail.com>
2016-05-30 13:38:43 -0400

    Finished exercises 0.2 0.3

---
+++ ch_0/ex0_2.cpp
@@ -0,0 +1,11 @@
+// Exercise 0.2 in Accelerated CPP
+// Author Cyrus Ramavarapu
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "This (\") is a  quote, and this (\\) is a backslash." << std::endl;
+
+	return 0;
+}
---
+++ ch_0/ex0_3.cpp
@@ -0,0 +1,12 @@
+//Exercise 0_3 in Accelerated CPP
+//Author: Cyrus Ramavarapu
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "     ." << std::endl;
+	std::cout << "\t." << std::endl;
+
+	return 0;
+}
