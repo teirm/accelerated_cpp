e9862e0dc99746b247808c640a17ec159f3803c6
teirm <tribunus.vii@gmail.com>
2016-08-22 12:45:02 -0400

    Grading schemes complete

--- ch_6/grading_schemes.cpp
+++ ch_6/grading_schemes.cpp
@@ -74,7 +74,14 @@ double optimistic_median(const Student_info& s)
 		return grade(s.midterm_grade, s.final_grade, median(nonzero));
 }
 
+double optimistic_median_analysis(const vector<Student_info>& students)
+{
+	vector<double> grades;
 
+	transform(students.begin(), students.end(),
+			  back_inserter(grades), optimistic_median);
+	return median(grades);
+}
 
 void write_analysis(ostream& out, const string& name,
 					double analysis(const vector<Student_info>&),
@@ -111,11 +118,11 @@ int main()
 		return 1;	
 	}
 
-	// do the anlyses
+	// do the analyses
 	write_analysis(cout, "median", median_analysis, did, didnt);
-//	write_analysis(cout, "median", average_analysis, did, didnt);
-//	write_analysis(cout, "median of homework turned in",
-//			       optimistic_median_analysis, did, didnt);
+	write_analysis(cout, "median", average_analysis, did, didnt);
+	write_analysis(cout, "median of homework turned in",
+			       optimistic_median_analysis, did, didnt);
 	
 	return 0;
 }
